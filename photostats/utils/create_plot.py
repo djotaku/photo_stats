66be87e6280b841e10f961034914fa96153e935d
Eric Mesa <ericsbinaryworld@gmail.com>
2020-03-22 15:26:09 -0400

    :chart_with_downwards_trend: Plot Fixes
    It now will make room for the labels. It also has optional params for labeling axis and the graph.

--- photostats/utils/create_plot.py
+++ photostats/utils/create_plot.py
@@ -3,10 +3,12 @@
 import matplotlib.pyplot as plt  # type: ignore
 
 
-def create_plot(group_names: list, group_data: list):
+def create_plot(group_names: list, group_data: list, x_label: str = "", y_label: str = "", title: str = ""):
     plt.style.use('fivethirtyeight')
+    plt.rcParams.update({'figure.autolayout': True})
     fig, ax = plt.subplots()
     ax.bar(group_names, group_data)
-    xlabels = ax.get_xticklabels()
-    plt.setp(xlabels, rotation=45, horizontalalignment='right')
+    x_labels = ax.get_xticklabels()
+    plt.setp(x_labels, rotation=45, horizontalalignment='right')
+    ax.set(xlabel=x_label, ylabel=y_label, title=title)
     plt.show()
